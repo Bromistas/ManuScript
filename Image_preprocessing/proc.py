0f9f410a43c10f0a55b27aac2d971ed7afdf651a
hmrguez <zealot.algo@gmail.com>
2024-07-06 20:16:28 -0400

    Basic cleanup

--- Image_preprocessing/proc.py
+++ Image_preprocessing/proc.py
@@ -2,50 +2,52 @@ import cv2
 import numpy as np
 import os
 
+
 def proc(Image, name):
     # Cargar la imagen en escala de grises
     image = cv2.imread(Image, cv2.IMREAD_GRAYSCALE)
 
-    #-NOT USED FILTERS-#
+    # -NOT USED FILTERS-#
 
     # Gaussian Blur
-    #gaussian_Blur = cv2.GaussianBlur(image, (5, 5), 0)
+    # gaussian_Blur = cv2.GaussianBlur(image, (5, 5), 0)
 
     # Median Blur
-    #median_Blur = cv2.medianBlur(image, 5)
+    # median_Blur = cv2.medianBlur(image, 5)
 
     # Mean Filter
-    #kernel = np.ones((5,5), np.float32) / 25
-    #mean_Filter = cv2.filter2D(image, -1, kernel)
+    # kernel = np.ones((5,5), np.float32) / 25
+    # mean_Filter = cv2.filter2D(image, -1, kernel)
 
     # Laplacian Filter
-    #laplacian_Filter = cv2.Laplacian(image, cv2.CV_64F)
-    #laplacian_abs = cv2.convertScaleAbs(laplacian)
+    # laplacian_Filter = cv2.Laplacian(image, cv2.CV_64F)
+    # laplacian_abs = cv2.convertScaleAbs(laplacian)
 
     # Binarization
-    #_, binarized = cv2.threshold(gaussian_Blur, 185, 255, cv2.THRESH_BINARY)
+    # _, binarized = cv2.threshold(gaussian_Blur, 185, 255, cv2.THRESH_BINARY)
 
     # Salt and Pepper
-    #kernel = np.ones((2,2), np.uint8)
-    #kernel2 = np.ones((2,2), np.uint8)
-    #close = cv2.morphologyEx(binarized, cv2.MORPH_CLOSE, kernel)
-    #open = cv2.morphologyEx(close, cv2.MORPH_OPEN, kernel2)
+    # kernel = np.ones((2,2), np.uint8)
+    # kernel2 = np.ones((2,2), np.uint8)
+    # close = cv2.morphologyEx(binarized, cv2.MORPH_CLOSE, kernel)
+    # open = cv2.morphologyEx(close, cv2.MORPH_OPEN, kernel2)
 
     # Dilatation
-    #kernel = np.ones((2,2), np.uint8)
-    #dilated = cv2.dilate(open, kernel, iterations = 1)
-    #dilated2 = cv2.dilate(binarized, kernel, iterations = 1)
+    # kernel = np.ones((2,2), np.uint8)
+    # dilated = cv2.dilate(open, kernel, iterations = 1)
+    # dilated2 = cv2.dilate(binarized, kernel, iterations = 1)
 
-    #-PREPROCESSING-#
+    # -PREPROCESSING-#
 
     # Bilateral Filter
     bilateral = cv2.bilateralFilter(image, 9, 75, 75)
-    
+
     # Adaptative Binarization
     binarized = cv2.adaptiveThreshold(bilateral, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 4)
 
     # Save the Results
     cv2.imwrite(f'Out/{name}', binarized)
+
 
 def proc1(Image, name):
     # Cargar la imagen en escala de grises
@@ -53,7 +55,7 @@ def proc1(Image, name):
 
     # Bilateral Filter
     bilateral = cv2.bilateralFilter(image, 9, 75, 75)
-    
+
     # Adaptative Binarization
     binarized = cv2.adaptiveThreshold(bilateral, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 4)
 
@@ -67,14 +69,14 @@ def proc2(Image, name):
 
     # Bilateral Filter
     bilateral = cv2.bilateralFilter(image, 9, 75, 75)
-    
+
     # Adaptative Binarization
     binarized = cv2.adaptiveThreshold(bilateral, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 4)
 
     # Dilatation
-    kernel = np.ones((2,2), np.uint8)
-    #dilated = cv2.dilate(open, kernel, iterations = 1)
-    dilated = cv2.dilate(binarized, kernel, iterations = 1)
+    kernel = np.ones((2, 2), np.uint8)
+    # dilated = cv2.dilate(open, kernel, iterations = 1)
+    dilated = cv2.dilate(binarized, kernel, iterations=1)
 
     # Save the Results
     cv2.imwrite(f'Out/{name}', dilated)
@@ -93,19 +95,20 @@ def proc3(Image, name):
     # Save the Results
     cv2.imwrite(f'Out/{name}', binarized)
 
+
 def proc4(Image, name):
     # Cargar la imagen en escala de grises
     image = cv2.imread(Image, cv2.IMREAD_GRAYSCALE)
 
     # Bilateral Filter
     bilateral = cv2.bilateralFilter(image, 9, 75, 75)
-    
+
     # Adaptative Binarization
     binarized = cv2.adaptiveThreshold(bilateral, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 4)
 
     # Salt and Pepper
-    kernel = np.ones((2,2), np.uint8)
-    kernel2 = np.ones((2,2), np.uint8)
+    kernel = np.ones((2, 2), np.uint8)
+    kernel2 = np.ones((2, 2), np.uint8)
     close = cv2.morphologyEx(binarized, cv2.MORPH_CLOSE, kernel)
     open = cv2.morphologyEx(close, cv2.MORPH_OPEN, kernel2)
 
@@ -115,7 +118,7 @@ def proc4(Image, name):
 
 # Original images's path
 Path = '../dataset/img'
-#Path = 'Img'
+# Path = 'Img'
 Images = [File for File in os.listdir(Path) if File.endswith(('.png', '.jpg', '.jpeg'))]
 
 # Process all images
--- model/Metrics.py
+++ model/Metrics.py
@@ -1,13 +1,15 @@
 import jiwer
 
+
 def Calculate_WER(reference: list[str], hypothesis: list[str]) -> float:
     """Calculates WER metric for one or multiple transcriptions"""
-    
+
     wer = jiwer.wer(reference, hypothesis)
     return wer
 
+
 def Calculate_CER(reference: list[str], hypothesis: list[str]) -> float:
     """Calculates CER metric for one or multiple transcription"""
-    
+
     cer = jiwer.cer(reference, hypothesis)
     return cer
